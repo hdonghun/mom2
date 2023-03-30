269990525bbcb996529d07cd9256e9572cdf24ad
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-03-30 22:21:23 +0900

    회원가입 및 로그인을 완료하고, 메인 페이지로 redirect성공 / redirect를 성공했을떄, alert로 알림창을 사용하려고 했는데, 실패 / AWS를 이용해서 Mysql데이터베이스 사용 및 연결 완료

--- src/main/java/hello/mom2/member/controller/MemberController.java
+++ src/main/java/hello/mom2/member/controller/MemberController.java
@@ -1,12 +1,12 @@
 package hello.mom2.member.controller;
 
 import hello.mom2.member.domain.Member;
-import hello.mom2.member.dto.request.MemberRequestDto;
-import hello.mom2.member.service.MemberService;
 import hello.mom2.member.service.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpSession;
@@ -21,7 +21,6 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     @GetMapping("/member/signup")
     public String getSign(){
@@ -31,8 +30,9 @@ public class MemberController {
     public String postSignup(Member member) {
         // 회원가입 처리 로직
         memberService.sign(member);
-        // redirect URL 반환
-        return "index";
+
+//        return "redirect:/";
+        return "redirect:/?signup_success=true";
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,6 +58,11 @@ public class MemberController {
         return "redirect:";
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    @RequestMapping("/redirect")
+    public RedirectView redirectExample() {
+        return new RedirectView("http://localhost:8080/");
+    }
 
 
 
--- src/main/java/hello/mom2/member/domain/Member.java
+++ src/main/java/hello/mom2/member/domain/Member.java
@@ -4,15 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 
-@Entity
 @Getter
 @Setter
+@Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false)
+
     private String password;
+    @Column(nullable = false, unique = true)
+
     private String email;
 
     public Long getId() { return id; }
